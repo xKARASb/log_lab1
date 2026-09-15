e45007b526728601590ed4eea013992e2ac81b9d
Maxim <maxkolosda@yandex.ru>
2026-09-15 07:37:12 +0300

    Additional task complete

--- task4.c
+++ task4.c
@@ -4,31 +4,34 @@
 
 int main(void) {
     srand(time(NULL));    
-    int m, n, f_s;
-    printf("размер массива m n:\n");
-    scanf("%d %d", &m, &n);
+    int m, f_s;
+    printf("размер массива m:\n");
+    scanf("%d", &m);
     int **matrix = malloc(m * sizeof(int*));
     
     printf("matrix:\n");
     for(int i = 0; i < m; ++i){
-        matrix[i] = malloc(n * sizeof(int));
-        for (int j = 0; j < n; ++j) {
+        matrix[i] = malloc(m * sizeof(int));
+        for (int j = 0; j < m; ++j) {
             matrix[i][j] = rand()%10;
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 
-    printf("sums colms enter 0, sums rows enter 1:\n");
-    scanf("%d", &f_s);
-    for (int i = 0; i < n; ++i){
-        int sum = 0;
-        for (int j = 0; j < m; ++j) {
-            if (f_s == 0) sum += matrix[j][i];
-            else sum += matrix[i][j];
+    // printf("sums colms enter 0, sums rows enter 1:\n");
+    // scanf("%d", &f_s);
+    int sum = 0;
+    for (int i = 0; i < m; ++i){
+        for (int j = 0; j < m-i-1; ++j) {
+            sum += matrix[i][j];
+            // if (f_s == 0) sum += matrix[j][i];
+            // else sum += matrix[i][j];
+            // printf("%d ", matrix[i][j]);
         }
-        printf("%d ", sum);
+        // printf("\n");
     }
+    printf("%d ", sum);
     printf("\n");
         
 
--- task5.c
+++ task5.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>   
 
-#define LEN 10
-
 struct Student {
     char famil[20];
     char name[20];
@@ -11,42 +9,142 @@ struct Student {
     int n_zach;
 };
 
+struct StudentNode {
+    struct Student current;
+    struct StudentNode* next;
+};
+
+void addStudent(struct StudentNode **head, char famil[], char name[], char facult[], int n_zach) {
+    struct StudentNode *newNode = (struct StudentNode*)malloc(sizeof(struct StudentNode));
+    if (!newNode) return;
+
+    strncpy(newNode->current.famil, famil, sizeof(newNode->current.famil) - 1);
+    strncpy(newNode->current.name, name, sizeof(newNode->current.name) - 1);
+    strncpy(newNode->current.facult, facult, sizeof(newNode->current.facult) - 1);
+    newNode->current.n_zach = n_zach;
+    newNode->next = NULL;
+
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    struct StudentNode *temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void inpDynList(struct StudentNode **head) {
+    char name[20], famil[20], facult[20];
+    char buffer[32];
+    int n_zach;
+
+    printf("Введите структуры:\n");
+    
+    for (;;) {
+        printf("Новый студент, для завершения введите \"*\":\n");
+        
+        printf("Имя: ");
+        if (!fgets(name, sizeof(name), stdin)) break;
+        name[strcspn(name, "\n")] = '\0';
+        
+        if (strcmp(name, "*") == 0) {
+            printf("Ввод завершен.\n");
+            break;
+        }
+
+        printf("Фамилия: ");
+        fgets(famil, sizeof(famil), stdin);
+        famil[strcspn(famil, "\n")] = '\0';
+    
+        printf("Факультет: ");
+        fgets(facult, sizeof(facult), stdin);
+        facult[strcspn(facult, "\n")] = '\0';
+        
+        printf("Номер зачётки: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        sscanf(buffer, "%d", &n_zach);
+
+        addStudent(head, famil, name, facult, n_zach);
+        printf("\n");
+    }
+}
+
+void outputList(struct StudentNode *head) {
+    struct StudentNode *current = head;
+    printf("\n--- Список студентов ---\n");
+    while (current != NULL) {
+        printf("Имя: %s | Фамилия: %s | Факультет: %s | Зачетка: %d\n", 
+               current->current.name, 
+               current->current.famil, 
+               current->current.facult, 
+               current->current.n_zach);
+        current = current->next;
+    }
+}
+
+void freeList(struct StudentNode *head) {
+    struct StudentNode *temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+void search(struct StudentNode *head, const char *query) {
+    if (head == NULL || query == NULL || strlen(query) == 0) {
+        printf("\nЗапрос для поиска пуст или список свободен.\n");
+        return;
+    }
+
+    struct StudentNode *current = head;
+    char n_zach_str[32];
+    int found_count = 0;
+
+    printf("\n--- Результаты поиска по запросу \"%s\" ---\n", query);
+
+    for (;current != NULL;) {
+        snprintf(n_zach_str, sizeof(n_zach_str), "%d", current->current.n_zach);
+
+        if (strstr(current->current.famil, query) != NULL ||
+            strstr(current->current.name, query) != NULL ||
+            strstr(current->current.facult, query) != NULL ||
+            strstr(n_zach_str, query) != NULL) 
+        {
+            printf("Имя: %-15s | Фамилия: %-15s | Факультет: %-10s | Зачетка: %d\n", 
+                   current->current.name, 
+                   current->current.famil, 
+                   current->current.facult, 
+                   current->current.n_zach);
+            found_count++;
+        }
+        current = current->next;
+    }
+
+    if (found_count == 0) {
+        printf("Совпадений не найдено.\n");
+    } else {
+        printf("Найдено записей: %d\n", found_count);
+    }
+}
 int main(void) {
-    struct Student stds[LEN] = {
-        {"ivanov",    "petr",  "math",     111111},
-        {"volosunin", "egor",  "ivt",      222222},
-        {"kashkin",   "ilia",  "ivt",      333333},
-        {"andreeva",  "anna",  "biology",  444444},
-        {"ivanov",    "oleg",  "math",     555555},
-        {"volosunin", "egor",  "ivt",      666666},
-        {"kashkin",   "ilia",  "ivt",      777777},
-        {"andreeva",  "anna",  "biology",  888888},
-        {"kashkin",   "petr",  "math",     111111},
-        {"volosunin", "egor",  "ivt",      222222}
-    };
+    struct StudentNode *head = NULL;
+    char query[32];
 
-    struct Student ss;
+    inpDynList(&head);
+    outputList(head);
 
-    printf("введите параметры для поиска или 0 для игнорирования параметра:\n");
-    printf("фамилия: ");
-    scanf("%19s", ss.famil);
-    printf("имя: ");
-    scanf("%19s", ss.name);
-    printf("факультет: ");
-    scanf("%19s", ss.facult);
-    printf("номер зачетки: ");
-    scanf("%d", &ss.n_zach);
-
-    printf("совпадения: \n");
-
-    for (int i = 0; i < LEN; ++i) {
-        if ((ss.famil[0] == '0' || strcmp(ss.famil, stds[i].famil) == 0) &&
-            (ss.name[0]  == '0' || strcmp(ss.name,  stds[i].name)  == 0) &&
-            (ss.facult[0]== '0' || strcmp(ss.facult,stds[i].facult)== 0) &&
-            (ss.n_zach   == 0   || ss.n_zach == stds[i].n_zach)){
-            printf("%s %s %s %d\n",
-                   stds[i].famil, stds[i].name,
-                   stds[i].facult, stds[i].n_zach);
+    if (head != NULL) {
+        printf("\nВведите фрагмент для поиска: ");
+        if (fgets(query, sizeof(query), stdin)) {
+            query[strcspn(query, "\n")] = '\0';
+            search(head, query);
         }
     }
+
+    freeList(head);
+    return 0;
 }
